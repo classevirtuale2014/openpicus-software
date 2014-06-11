7ce5615a88d6e4c2e28c84531dc8d1134150c533
Spallac <spallac96@gmail.com>
2014-06-11 14:15:37 +0200

    openpicus lettura sensori
    Leggo i valori dei sensori. Da aggiungere parte comunicazione.

---
+++ c.v. openpicus/Libs/ExternalLib/Include/README.md
@@ -0,0 +1,4 @@
+lib_grove_input_capture
+=======================
+
+The library can be used as "helper library" with sensor using the input capture module of the Flyport microcontroller or as standalone library, to easily access the input capture module of the Flyport's microcontroller. More infos and examples on wiki.openpicus.com 
---
+++ c.v. openpicus/Libs/ExternalLib/Include/rht03.h
@@ -0,0 +1,60 @@
+ /** \file rht03.h
+ *  \brief Grove devices support library 
+ */
+
+/**
+\addtogroup Grove devices
+@{
+*/
+
+/* **************************************************************************																					
+ *                                OpenPicus                 www.openpicus.com
+ *                                                            italian concept
+ * 
+ *            openSource wireless Platform for sensors and Internet of Things	
+ * **************************************************************************
+ *  FileName:        rht03.h
+ *  Module:          FlyPort WI-FI/EHT
+ *  Compiler:        Microchip C30 v3.12 or higher
+ *
+ *  Author               Rev.    Date              Comment
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Davide Vicca    1.0     11/17/2012		   First release  (core team)
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  Software License Agreement
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  This is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License (version 2) as published by 
+ *  the Free Software Foundation AND MODIFIED BY OpenPicus team.
+ *  
+ *  ***NOTE*** The exception to the GPL is included to allow you to distribute
+ *  a combined work that includes OpenPicus code without being obliged to 
+ *  provide the source code for proprietary components outside of the OpenPicus
+ *  code. 
+ *  OpenPicus software is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details. 
+ * 
+ * 
+ * Warranty
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * THE SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ * LIMITATION, ANY WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * WE ARE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF
+ * PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
+ * BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE
+ * THEREOF), ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER
+ * SIMILAR COSTS, WHETHER ASSERTED ON THE BASIS OF CONTRACT, TORT
+ * (INCLUDING NEGLIGENCE), BREACH OF WARRANTY, OR OTHERWISE.
+ *
+ **************************************************************************/
+
+extern const void *RHT03;
+
+#define HUMD		0
+#define TEMP		1
